b50c9ccf86e97534e4e13f22df8340d9080a745e
Gerard <1304274443@qq.com>
2019-03-18 23:29:09 +0800

    新增殊文组，简化字符串格式化

--- app.js
+++ app.js
@@ -3,74 +3,66 @@ const fs = require("fs");
 const requestLib = require("request");
 const app = http.createServer();
 const ytdl = require('ytdl-core');
+const mysql = require('mysql');
 
 var address = "http://localhost:5700/send_group_msg?";
-var debug = true;
+var debug = false;
 var grep = /^@[\S\s]*/;
+var db = {
+    host: 'localhost',
+    user: 'root',
+    password: '12shf1',
+    database: 'qqbot'
+};
 
 var groups = new Array();
-groups["AIC"] = ["190674121", "118047250", "600132151"];
-groups["LUNA"] = ["151751140", "118047250", "600132151", "739889796"];
-groups["AKARI"] = ["614751169", "118047250", "600132151"];
-groups["SIRO"] = ["118047250", "600132151"];
+groups["AIC"] = ["190674121", "118047250"];
+groups["LUNA"] = ["118047250", "739889796"];
+groups["AKARI"] = ["614751169", "118047250"];
+groups["SIRO"] = ["118047250"];
 groups["AOI"] = ["634725864"];
-groups["PPH"] = ["151751140", "118047250", "600132151", "739889796"];
+groups["PPH"] = ["118047250", "739889796"];
+groups["IKUNA"] = ["739889796", "118047250"];
+groups["kotobumi"] = ["776234515"];
 
-function getUrl(msg, type) {
-    var postdata = new Array();
-    for (var i = 0, len = groups[type].length; i < len; i++) {
-        postdata[i] = address + "group_id=" + groups[type][i] + "&message=" + encodeURIComponent(msg);
-    }
-    return postdata;
-}
-
-/*ytdl.getInfo(url, (err, info) => {
-    if (err) {
-        console.error(err.message);
-        process.exit(1);
-        return;
-    } else {
-        let output = opts.output;
-        let ext = (output || '').match(/(\.\w+)?$/)[1];
-
-        if (output) {
-            if (ext && !opts.quality && !opts.filterContainer) {
-                opts.filterContainer = '^' + ext.slice(1) + '$';
-                console.log(opts.filterContainer);
+function send(type, send_text) {
+    var conn = mysql.createConnection(db);
+    conn.connect();
+    for (let i = 0, len = groups[type].length; i < len; i++) {
+        conn.query('select * from pause where group_id="' + groups[type][i] + '"', (err, result) => {
+            if (result[0]['is_pause'] == undefined || result[0]['is_pause']) {
+                console.log('pause');
+                return null;
+            } else {
+                let text = `${address}group_id=${groups[type][i]}&message=${encodeURIComponent(send_text)}`;
+                try {
+                    debug ? console.log(text) : requestLib(text);
+                } catch (error) {
+                    console.log(error);
+                }
             }
-        } else if (process.stdout.isTTY) {
-            output = '{title}';
-            console.log(output);
-        }
+        });
     }
-});
-
-
-function downloadVideo(url) {
-    if (ytdl.validateURL(url)) {
-        ytdl(url, { filter: (format) => format.container === 'mp4' })
-            .pipe(fs.createWriteStream('video.mp4'));
-    }
+    conn.end();
 }
-*/
 app.on('request', function(req, res) {
-    var post = '';
+    let post = '';
     req.on('data', function(chunk) {
         post += chunk;
     });
 
     req.on('end', function() {
-        var dataDec = post;
+        let dataDec = post;
         console.log(dataDec);
         if (dataDec != (null | undefined | '')) {
-            var content = JSON.parse(dataDec);
-            var title = content.Title;
-            var postby = content.postBy;
-            var postat = content.postat;
-            var url = content.url;
-            var type = content.type;
-            var text = content.text;
-            var from = content.from;
+            let content = JSON.parse(dataDec);
+            let title = content.Title;
+            let postby = content.postBy;
+            let postat = content.postat;
+            let url = content.url;
+            let type = content.type;
+            let text = content.text;
+            let from = content.from;
             if (text && grep.test(text)) {
                 res.writeHead(403, { 'Content-type': 'text/html' });
                 res.end();
@@ -81,71 +73,62 @@ app.on('request', function(req, res) {
                 res.writeHead(200, { 'Content-type': 'text/html' });
                 switch (from) {
                     case 'video':
-                        var send_text =
-                            postby + "|更新了：\n" +
-                            "--------------------------------------------------\n" +
-                            title +
-                            "\n--------------------------------------------------\n" +
-                            "视频地址：" + url +
-                            "\n--------------------------------------------------\n" +
-                            "更新于：" + postat +
-                            "\n--------------------------------------------------"
-                            /* +
-                                                        "\n提示：可使用-f:zh:xxxxxxx来翻译"*/
-                        ;
+                        let send_text = `
+                        ${postby} |更新了：
+                        \n--------------------------------------------------\n 
+                        ${title}
+                        \n--------------------------------------------------\n
+                        视频地址：${url}
+                        \n--------------------------------------------------\n 
+                        更新于：${postat}
+                        \n--------------------------------------------------\n
+                        提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                     case 'twitter':
-                        var send_text =
-                            postby + "|发推了：\n" +
-                            "--------------------------------------------------\n" +
-                            text +
-                            "\n--------------------------------------------------\n" +
-                            "地址：" + url +
-                            "\n--------------------------------------------------\n" +
-                            "发布于：" + postat +
-                            "\n--------------------------------------------------"
-                            /* +
-                                                        "\n提示：可使用-f:zh:xxxxxxx来翻译"*/
-                        ;
+                        let send_text = `
+                        ${postby} |更新了：
+                        \n--------------------------------------------------\n 
+                        ${text}
+                        \n--------------------------------------------------\n
+                        视频地址：${url}
+                        \n--------------------------------------------------\n 
+                        更新于：${postat}
+                        \n--------------------------------------------------\n
+                        提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                     default:
-                        var send_text =
-                            postby + "|更新了：\n" +
-                            "--------------------------------------------------\n" +
-                            title +
-                            "\n--------------------------------------------------\n" +
-                            "视频地址：" + url +
-                            "\n--------------------------------------------------\n" +
-                            "更新于：" + postat +
-                            "\n--------------------------------------------------"
-                            /* +
-                                                        "\n提示：可使用-f:zh:xxxxxxx来翻译"*/
-                        ;
+                        let send_text = `
+                            ${postby} |更新了：
+                            \n--------------------------------------------------\n 
+                            ${title}
+                            \n--------------------------------------------------\n
+                            视频地址：${url}
+                            \n--------------------------------------------------\n 
+                            更新于：${postat}
+                            \n--------------------------------------------------\n
+                            提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                 }
-                var urls = getUrl(send_text, type);
-                console.log(send_text);
-                for (var i = 0, len = urls.length; i < len; i++) {
-                    try {
-                        debug ? console.log(urls[i]) : requestLib(urls[i]);
-                    } catch (error) {
-                        console.error(error);
-                    }
+                //let urls = getUrl(send_text, type);
+                //console.log(send_text);
+                //console.log(urls);
 
-                }
+                send(type, send_text);
 
-                fs.writeFile('./data.log', "\n" + send_text + "\n", { flag: 'a+', encoding: 'utf-8', mode: '0666' }, function(err, fd) {
-                    if (err) { return console.error(err); }
-                });
-                fs.writeFile('./data.log', post, { flag: 'a+', encoding: 'utf-8', mode: '0666' }, function(err, fd) {
-                    if (err) { return console.error(err); }
-                });
-                res.write("200 OK");
-                res.end();
             }
+
+            fs.writeFile('./data.log', "\n" + send_text + "\n", { flag: 'a+', encoding: 'utf-8', mode: '0666' }, function(err, fd) {
+                if (err) { return console.error(err); }
+            });
+            fs.writeFile('./data.log', post, { flag: 'a+', encoding: 'utf-8', mode: '0666' }, function(err, fd) {
+                if (err) { return console.error(err); }
+            });
+            res.write("200 OK");
+            res.end();
+
         } else {
             fs.writeFile('./data.log', "\nerror\n", { flag: 'a+', encoding: 'utf-8', mode: '0666' });
-            res.writeHead(403, { 'Content-typr': 'text/html' });
+            res.writeHead(403, { 'Content-type': 'text/html' });
             res.write("error");
             res.end()
         }
@@ -153,3 +136,35 @@ app.on('request', function(req, res) {
     });
 });
 app.listen(8080, '0.0.0.0');
+
+const file = http.createServer();
+file.on('request', function(req, res) {
+            let post = '';
+            req.on('data', function(chunk) {
+                post += chunk;
+            });
+
+            req.on('end', function() {
+                        console.log(post);
+                        let content = JSON.parse(post);
+                        name = content.name;
+                        url = content.url;
+                        type = content.type;
+                        if (type == "jpeg" || type == "png" || type == "jpg" || type == "gif") {
+                            res.writeHead(403, { "content-type": "text/plain" });
+                            res.end();
+                        } else if (type == "flv" || type == "mp4") {
+                            res.writeHead(200, { "content-type": "text/plain" });
+                            let text = `${address}776234515&message=${encodeURIComponent(`有新文件上传到Onedrive了\n文件名：${name}\n直链url：${url}`)}`;
+            debug ? console.log(text) : requestLib(text);
+            res.end();
+        } else {
+            res.writeHead(403, { "content-type": "text/plain" });
+            res.end();
+        }
+    });
+});
+                
+
+
+file.listen(8081, '0.0.0.0');
