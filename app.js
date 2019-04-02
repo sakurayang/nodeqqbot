17cdcaaacad50a75cdfc5d7443d3d22561550d86
sakurayang <1304274443@qq.com>
2019-04-02 08:24:29 +0800

    加入直播表功能

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const http = require("http");
 const fs = require("fs");
 const requestLib = require("request");
 const app = http.createServer();
-const ytdl = require('ytdl-core');
 const mysql = require('mysql');
 const config = require("./config.json")
 
@@ -11,14 +10,21 @@ var debug = false;
 var grep = /^@[\S\s]*/;
 
 var groups = new Array();
+/*AIC字幕组群：190674121
+ *AIC粉丝1群：118047250
+ *富士葵粉丝群：634725864
+ *PHH粉丝群：739889796
+ *akebi群：776234515
+ */
 groups["AIC"] = ["190674121", "118047250"];
 groups["LUNA"] = ["118047250", "739889796"];
-groups["AKARI"] = ["614751169", "118047250"];
+groups["AKARI"] = ["118047250"];
 groups["SIRO"] = ["118047250"];
 groups["AOI"] = ["634725864"];
 groups["PPH"] = ["118047250", "739889796"];
 groups["IKUNA"] = ["739889796", "118047250"];
 groups["kotobumi"] = ["776234515"];
+groups['all'] = ["118047250", "739889796"]
 
 function send(type, send_text) {
     var conn = mysql.createConnection(config.db);
@@ -68,40 +74,13 @@ app.on('request', function(req, res) {
                 res.writeHead(200, { 'Content-type': 'text/html' });
                 switch (from) {
                     case 'video':
-                        let send_text = `
-                        ${postby} |更新了：
-                        \n--------------------------------------------------\n 
-                        ${title}
-                        \n--------------------------------------------------\n
-                        视频地址：${url}
-                        \n--------------------------------------------------\n 
-                        更新于：${postat}
-                        \n--------------------------------------------------\n
-                        提示：可使用!f:zh:xxxxxxx来翻译`;
+                        let send_text = `${postby} |更新了：\n--------------\n${title}\n--------------\n视频地址：${url}\n--------------\n更新于：${postat}\n--------------\n提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                     case 'twitter':
-                        let send_text = `
-                        ${postby} |更新了：
-                        \n--------------------------------------------------\n 
-                        ${text}
-                        \n--------------------------------------------------\n
-                        视频地址：${url}
-                        \n--------------------------------------------------\n 
-                        更新于：${postat}
-                        \n--------------------------------------------------\n
-                        提示：可使用!f:zh:xxxxxxx来翻译`;
+                        let send_text = `${postby} |发推了：\n--------------\n${text}\n--------------\n原文地址：${url}\n--------------\n更新于：${postat}\n--------------\n提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                     default:
-                        let send_text = `
-                            ${postby} |更新了：
-                            \n--------------------------------------------------\n 
-                            ${title}
-                            \n--------------------------------------------------\n
-                            视频地址：${url}
-                            \n--------------------------------------------------\n 
-                            更新于：${postat}
-                            \n--------------------------------------------------\n
-                            提示：可使用!f:zh:xxxxxxx来翻译`;
+                        let send_text = `${postby} |更新了：\n--------------\n${title}\n--------------\n视频地址：${url}\n--------------\n更新于：${postat}\n--------------\n提示：可使用!f:zh:xxxxxxx来翻译`;
                         break;
                 }
                 //let urls = getUrl(send_text, type);
@@ -140,11 +119,12 @@ file.on('request', function(req, res) {
             });
 
             req.on('end', function() {
+                        res.writeHead(200);
                         console.log(post);
                         let content = JSON.parse(post);
-                        name = content.name;
-                        url = content.url;
-                        type = content.type;
+                        let name = content.name;
+                        let url = content.url;
+                        let type = content.type;
                         if (type == "jpeg" || type == "png" || type == "jpg" || type == "gif") {
                             res.writeHead(403, { "content-type": "text/plain" });
                             res.end();
@@ -159,7 +139,36 @@ file.on('request', function(req, res) {
         }
     });
 });
-                
-
-
 file.listen(8081, '0.0.0.0');
+
+weekdays = ['日', '一', '二', '三', '四', '五', '六', '日'];
+jpweekdays = ['日', '月', '火', '水', '木', '金', '土', '日'];
+const timeline = http.createServer();
+timeline.on('request', (req, res) => {
+    let post = '';
+    req.on('data', (chunk) => { post += chunk });
+    req.on('end', () => {
+        console.log(post);
+        let content = JSON.parse(post);
+        let date = new Date();
+        let year = date.getFullYear();
+        let month = date.getMonth() + 1;
+        let day = date.getDate();
+        let week = date.getDay();
+        let url = content.url;
+        let name = content.name;
+        let text = content.text;
+        let time = content.time;
+        if (name != "hololivetv") {
+            res.writeHead(403);
+            res.end()
+        } else {
+            res.writeHead(200,{"content-type":"text/plain"});
+            res.write("200 OK")
+            text = `各位早上好呀~\n今天是${year}年${month}月${day}日 星期${weekdays[week]} ${jpweekdays[week]}\n今天的hololive直播表如下\n▁▁▁▁▁▁▁▁▁▁\n${text}\n▔▔▔▔▔▔▔▔▔▔\n发布于${time}\n来源：${url}`
+            send('all', text);
+            res.end();
+        }
+    });
+});
+timeline.listen(8083, '0.0.0.0');
