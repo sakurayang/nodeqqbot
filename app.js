7158eda13b4ebf3d43abd043f1c9ed83b1410e2b
Gerard <1304274443@qq.com>
2019-04-06 05:41:59 +0800

    增加DD早报，发送图片

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const http = require("http");
-const fs = require("fs");
-const requestLib = require("request");
-const app = http.createServer();
-const mysql = require('mysql');
-const config = require("./config.json")
+fs = require("fs"),
+    requestLib = require("request"),
+    app = http.createServer(),
+    mysql = require('mysql'),
+    config = require("./config.json"), { registerFont, createCanvas } = require('canvas'),
+    emoji = require('node-emoji');
 
 var address = config.address;
 var debug = false;
 var grep = /^@[\S\s]*/;
+var date = new Date(),
+    year = date.getFullYear(),
+    month = date.getMonth() + 1,
+    day = date.getDate(),
+    week = date.getDay(),
+    weekdays = ['日', '一', '二', '三', '四', '五', '六', '日'],
+    jpweekdays = ['日', '月', '火', '水', '木', '金', '土', '日'];
+
+registerFont("./fonts/NotoSansCJKjp-Regular.ttf", { "family": "NotoSans" });
 
 var groups = new Array();
 /*AIC字幕组群：190674121
@@ -24,7 +34,7 @@ groups["AOI"] = ["634725864"];
 groups["PPH"] = ["118047250", "739889796"];
 groups["IKUNA"] = ["739889796", "118047250"];
 groups["kotobumi"] = ["776234515"];
-groups['all'] = ["118047250", "739889796"]
+groups['all'] = ["118047250", "739889796", "591845717"]
 
 function send(type, send_text) {
     var conn = mysql.createConnection(config.db);
@@ -35,9 +45,16 @@ function send(type, send_text) {
                 console.log('pause');
                 return null;
             } else {
-                let text = `${address}group_id=${groups[type][i]}&message=${encodeURIComponent(send_text)}`;
                 try {
-                    debug ? console.log(text) : requestLib(text);
+                    debug ? console.log(send_text) :
+                        requestLib({
+                            url: address,
+                            method: 'POST',
+                            json: {
+                                group_id: groups[type][i],
+                                message: send_text
+                            }
+                        });
                 } catch (error) {
                     console.log(error);
                 }
@@ -46,28 +63,109 @@ function send(type, send_text) {
     }
     conn.end();
 }
+
+function sendImage(html) {
+    let text = '';
+    text += html;
+    if (text == '') console.log("no value");
+    let split_text = emoji.replace(text, (emoji) => '').replace(/\ufffd/ig, '').split(' ');
+    split_text[1] = split_text[0] + split_text[1];
+    split_text.shift();
+    split_text.pop();
+    let line = split_text.length;
+
+    let text_width = new Array(),
+        width = new Array();
+    split_text.forEach((value, index) => {
+        text_width[index] = { "value": value, "width": value.length };
+        width[index] = value.length;
+    });
+    width.sort((a, b) => a - b);
+    let index = text_width.findIndex((value) => value.width == width[width.length - 1]);
+
+    /* Draw a canvas
+     *1 ┌────────────────────────────────────
+     *2 │                                     20px 
+     *3 │                Title                30px
+     *4 │-------------------------------------5 px---\
+     *5 │                                     15px---/ 
+     *6 │<=15px=>This is a Line               20px
+     *7 │        This is a Line                 │
+     *8 │        This is a Line                 │
+     *9 │        This is a Line                 │
+     *10│        This is the longest Line<=5px=>│
+     *11│                                     20px
+     *12└────────────────────────────────────
+     */
+    let a = new createCanvas(1, 1);
+    const b = a.getContext('2d');
+    b.font = '30px "NotoSans"';
+    /*只用一次所以变量名就随便了*/
+    let canvas_width = b.measureText(text_width[index].value).width + 20,
+        canvas_height = line * 30 + 60;
+    let canvas = new createCanvas(canvas_width, canvas_height);
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas_width, canvas_height);
+
+    ctx.font = '10px "NotoSans"';
+    ctx.textAlign = 'end';
+    ctx.textBaseline = 'bottom';
+    ctx.fillStyle = '#272727';
+    ctx.fillText("Power By node-canvas, program by Gerard V3.0", canvas_width, 10);
+
+    ctx.font = '30px "NotoSans"';
+    ctx.textAlign = 'center';
+    ctx.fillStyle = 'black';
+    ctx.fillText(split_text[0], canvas_width / 2, 60);
+
+    ctx.beginPath();
+    ctx.moveTo(10, 62);
+    ctx.lineTo(canvas_width - 10, 62);
+    ctx.stroke();
+
+    ctx.font = '20px "NotoSans"';
+    ctx.textAlign = 'start';
+    for (let i = 1, len = split_text.length; i < len; i++) {
+        const text = split_text[i];
+        let lineNum = i - 1;
+        ctx.fillText(text, 15, 100 + 25 * lineNum);
+    }
+
+    ctx.font = '10px "NotoSans"';
+    ctx.textAlign = 'end';
+    ctx.fillStyle = '#272727';
+    ctx.fillText(`${year}年${month}月${day}日 星期${weekdays[week]} ${jpweekdays[week]}曜日`, canvas_width, canvas_height);
+
+    var base64 = canvas.toDataURL().substr(22);
+    //console.log(base64);
+    send('all', `[CQ:image,file=base64://${base64}]`);
+}
+
+
 app.on('request', function(req, res) {
-    let post = '';
+    let dataDec
     req.on('data', function(chunk) {
-        post += chunk;
+        dataDec = chunk.toString();
     });
 
     req.on('end', function() {
-        let dataDec = post;
         console.log(dataDec);
         if (dataDec != (null | undefined | '')) {
             let content = JSON.parse(dataDec);
-            let title = content.Title;
-            let postby = content.postBy;
-            let postat = content.postat;
-            let url = content.url;
-            let type = content.type;
-            let text = content.text;
-            let from = content.from;
+            let title = content.Title,
+                postby = content.postBy,
+                postat = content.postat,
+                url = content.url,
+                type = content.type,
+                text = content.text,
+                from = content.from;
             if (text && grep.test(text)) {
                 res.writeHead(403, { 'Content-type': 'text/html' });
                 res.end();
-            } else if (!url | !type) {
+            } else
+            if (!url | !type) {
                 res.writeHead(503, { 'Content-type': 'text/html' });
                 res.end();
             } else {
@@ -111,62 +209,23 @@ app.on('request', function(req, res) {
 });
 app.listen(8080, '0.0.0.0');
 
-const file = http.createServer();
-file.on('request', function(req, res) {
-            let post = '';
-            req.on('data', function(chunk) {
-                post += chunk;
-            });
 
-            req.on('end', function() {
-                        res.writeHead(200);
-                        console.log(post);
-                        let content = JSON.parse(post);
-                        let name = content.name;
-                        let url = content.url;
-                        let type = content.type;
-                        if (type == "jpeg" || type == "png" || type == "jpg" || type == "gif") {
-                            res.writeHead(403, { "content-type": "text/plain" });
-                            res.end();
-                        } else if (type == "flv" || type == "mp4") {
-                            res.writeHead(200, { "content-type": "text/plain" });
-                            let text = `${address}776234515&message=${encodeURIComponent(`有新文件上传到Onedrive了\n文件名：${name}\n直链url：${url}`)}`;
-            debug ? console.log(text) : requestLib(text);
-            res.end();
-        } else {
-            res.writeHead(403, { "content-type": "text/plain" });
-            res.end();
-        }
-    });
-});
-file.listen(8081, '0.0.0.0');
-
-weekdays = ['日', '一', '二', '三', '四', '五', '六', '日'];
-jpweekdays = ['日', '月', '火', '水', '木', '金', '土', '日'];
 const timeline = http.createServer();
 timeline.on('request', (req, res) => {
-    let post = '';
-    req.on('data', (chunk) => { post += chunk });
+    let content
+    req.on('data', (chunk) => {
+        content = JSON.parse(chunk.toString());
+    });
     req.on('end', () => {
-        console.log(post);
-        let content = JSON.parse(post);
-        let date = new Date();
-        let year = date.getFullYear();
-        let month = date.getMonth() + 1;
-        let day = date.getDate();
-        let week = date.getDay();
-        let url = content.url;
-        let name = content.name;
-        let text = content.text;
-        let time = content.time;
+        let name = content.name,
+            text = emoji.replace(content.text, (emoji) => '');
         if (name != "hololivetv") {
             res.writeHead(403);
             res.end()
         } else {
-            res.writeHead(200,{"content-type":"text/plain"});
+            res.writeHead(200, { "content-type": "text/plain" });
             res.write("200 OK")
-            text = `各位早上好呀~\n今天是${year}年${month}月${day}日 星期${weekdays[week]} ${jpweekdays[week]}\n今天的hololive直播表如下\n▁▁▁▁▁▁▁▁▁▁\n${text}\n▔▔▔▔▔▔▔▔▔▔\n发布于${time}\n来源：${url}`
-            send('all', text);
+            sendImage(text)
             res.end();
         }
     });
